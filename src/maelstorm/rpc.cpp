97c36c6614972d063aecdda71d0f61d2783524a0
Nirmesh Khandelwal <nirmesh.khandelwal@gmail.com>
2024-09-03 17:03:39 -0700

    Cleanup - Move maelstorm to its own separate directory

---
+++ src/maelstorm/rpc.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+#include <nlohmann/json.hpp>
+
+using namespace std;
+using json = nlohmann::json;
+
+struct Body {
+    std::string type;
+    Body(const json& j) : type(j["type"].get<std::string>()) {}
+};
+
+class Message {
+public:
+    string src;
+    string dest;
+    optional<Body> body;
+
+    Message(const json& j) {
+        src = j["src"].get<std::string>();
+        dest = j["dest"].get<std::string>();
+        if (j.contains("body")) {
+            body = Body(j["body"]);
+        }
+    }
+};
